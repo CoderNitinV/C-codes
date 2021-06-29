60d8538537d89236a60bf81157ad59a3682e8997
CoderNitinV <86653011+CoderNitinV@users.noreply.github.com>
2021-06-29 14:50:08 +0530

    Add files via upload

---
+++ sumoffirstNno.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int n,sum;
+cout<<"Enter the No. of your choice"<<endl;
+cin>>n;
+
+sum =n*(n+1)/2;
+cout<<"The sum of n Natural No. is ="<<sum;
+return 0;
+
+
+}
