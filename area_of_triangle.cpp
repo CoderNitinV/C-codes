f68439a302ac136d496eaee76d3d2315c3ab616b
CoderNitinV <86653011+CoderNitinV@users.noreply.github.com>
2021-06-29 14:00:57 +0530

    Add files via upload

---
+++ area_of_triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//#include<cmath>
+
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"Write base and hieght of a Equilateral triangle"<<endl;
+    cin>>a>>b;
+    c =(a*b)/2;
+    cout<<"the area of triangle is ="<<c;
+    return 0;
+
+}
---
+++ sizeofdata.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void main();
+{
+    INT_MAX;
+
+cout<<INT_MAX<<endl;
+return 0;
+
+}
